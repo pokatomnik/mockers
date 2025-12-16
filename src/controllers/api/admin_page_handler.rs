eae88471f878b02274ade632cfcf01d9ac785bd9
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2025-12-16 17:47:13 +0300

    Cleanup

--- src/controllers/api/admin_page_handler.rs
+++ src/controllers/api/admin_page_handler.rs
@@ -1,13 +1,10 @@
-use crate::server::mockers_context::MockersContext;
 use http_body_util::Full;
 use hyper::body::Bytes;
 use hyper::{Request, Response, StatusCode};
-use routerify_ng::ext::RequestExt;
 use std::convert::Infallible;
-use std::sync::Arc;
 
 pub async fn admin_page_handler(
-    req: Request<Full<Bytes>>,
+    _req: Request<Full<Bytes>>,
 ) -> Result<Response<Full<Bytes>>, Infallible> {
     return Ok(Response::builder()
         .status(StatusCode::OK)
