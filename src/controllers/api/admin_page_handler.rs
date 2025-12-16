8c9e6c45058d771060eed5ecbcb896232e8c81e8
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2025-12-16 17:46:12 +0300

    Added admin API for listing all mocks, listing mocks by path and create a single mock

---
+++ src/controllers/api/admin_page_handler.rs
@@ -0,0 +1,21 @@
+use crate::server::mockers_context::MockersContext;
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::{Request, Response, StatusCode};
+use routerify_ng::ext::RequestExt;
+use std::convert::Infallible;
+use std::sync::Arc;
+
+pub async fn admin_page_handler(
+    req: Request<Full<Bytes>>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
+    let context = req
+        .data::<Arc<MockersContext>>()
+        .map(|c| c.clone().response_cache.clone());
+    
+    return Ok(Response::builder()
+        .status(StatusCode::OK)
+        // TODO send SPA HTML here
+        .body(Full::new(Bytes::from("Hello world!")))
+        .unwrap());
+}
