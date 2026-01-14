45f9547b87ea9eac1cfaa8a54ca67f783f28d451
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2026-01-14 16:56:11 +0300

    refactor get_all_mocks controller (#62)

--- src/controllers/api/get_all_mocks.rs
+++ src/controllers/api/get_all_mocks.rs
@@ -16,39 +16,34 @@ pub async fn get_all_mocks(req: Request<Full<Bytes>>) -> Result<Response<Full<By
     let response_cache = req
         .data::<Arc<MockersContext>>()
         .map(|c| c.clone().response_cache.clone());
-    match response_cache {
-        Some(cache) => {
-            let all_mocks = cache.get_all().await;
-            let json = serde_json::to_string(
-                &Ok::<HashMap<String, HashMap<String, CachedResponse>>, String>(all_mocks)
-                    .to_protocol(),
-            );
-            let status_code = json
-                .as_ref()
-                .map(|_| StatusCode::OK)
-                .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
-            let bytes = json
-                .map(|str| Bytes::from(str))
-                .unwrap_or(Bytes::from(Bytes::new()));
-            Ok(Response::builder()
-                .status(status_code)
-                .header(CONTENT_TYPE, APPLICATION_JSON)
-                .body(Full::new(bytes))
-                .unwrap_or(Response::default()))
-        }
-        None => {
-            let json = serde_json::to_string(
-                &Err::<HashMap<String, HashMap<String, CachedResponse>>, String>(
-                    MockersErrors::GetAllMocksFailed.to_string(),
-                )
-                .to_protocol(),
-            );
-            let bytes = json.map(|str| Bytes::from(str)).unwrap_or(Bytes::new());
-            Ok(Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .header(CONTENT_TYPE, APPLICATION_JSON)
-                .body(Full::new(Bytes::from(bytes)))
-                .unwrap_or(Response::default()))
-        }
-    }
+
+    let Some(cache) = response_cache else {
+        let json = serde_json::to_string(
+            &Err::<HashMap<String, HashMap<String, CachedResponse>>, String>(
+                MockersErrors::GetAllMocksFailed.to_string(),
+            )
+            .to_protocol(),
+        );
+        let bytes: Bytes = json.map(|str| str.into()).unwrap_or_default();
+        return Ok(Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .header(CONTENT_TYPE, APPLICATION_JSON)
+            .body(bytes.into())
+            .unwrap_or_default());
+    };
+
+    let all_mocks = cache.get_all().await;
+    let json = serde_json::to_string(
+        &Ok::<HashMap<String, HashMap<String, CachedResponse>>, String>(all_mocks).to_protocol(),
+    );
+    let status_code = json
+        .as_ref()
+        .map(|_| StatusCode::OK)
+        .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
+    let bytes: Bytes = json.map(|str| str.into()).unwrap_or_default();
+    Ok(Response::builder()
+        .status(status_code)
+        .header(CONTENT_TYPE, APPLICATION_JSON)
+        .body(bytes.into())
+        .unwrap_or_default())
 }
