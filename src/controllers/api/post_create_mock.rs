76872b63463eb65f09b0d5614a71d0b6e91f1315
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2026-01-14 15:03:08 +0300

    Refactor post_create_mock controller

--- src/controllers/api/post_create_mock.rs
+++ src/controllers/api/post_create_mock.rs
@@ -31,79 +31,75 @@ pub async fn post_create_mock(
         .data::<Arc<MockersContext>>()
         .map(|c| c.clone().response_cache.clone());
 
-    match response_cache {
-        Some(cache) => {
-            let body_bytes = req
-                .body()
-                .clone()
-                .collect()
-                .await
-                .unwrap_or_default()
-                .to_bytes();
-            let body_str = String::from_utf8(body_bytes.to_vec()).unwrap_or(String::new());
-            let create_mock_params = serde_json::from_str::<CreateMockParams>(&body_str);
+    let Some(cache) = response_cache else {
+        let json = serde_json::to_string(
+            &Err::<HashMap<String, HashMap<String, CachedResponse>>, String>(
+                MockersErrors::AddMockFailed.to_string(),
+            )
+            .to_protocol(),
+        );
+        let bytes = json.map(|s| Bytes::from(s)).unwrap_or(Bytes::new());
+        return Ok(Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .header(CONTENT_TYPE, APPLICATION_JSON)
+            .body(Full::new(Bytes::from(bytes)))
+            .unwrap_or(Response::default()));
+    };
 
-            match create_mock_params {
-                Ok(mock_params) => {
-                    // do not wait, respond immediately
-                    let path = mock_params.path.clone();
-                    let method = mock_params.method.clone();
-                    tokio::spawn(async move {
-                        let response = &CachedResponse::new(
-                            mock_params.status_code,
-                            mock_params.delay_ms.unwrap_or(0),
-                            mock_params.headers,
-                            mock_params.body,
-                        );
-                        cache.upsert(path, method, response).await;
-                    });
-                    let json = serde_json::to_string(
-                        &Ok::<String, String>(format!(
-                            "Mock inserted for path: '{}' and method: '{}'",
-                            mock_params.path, mock_params.method
-                        ))
-                        .to_protocol(),
-                    );
-                    let status = json
-                        .as_ref()
-                        .map(|_| StatusCode::OK)
-                        .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
-                    let bytes = json.map(|str| Bytes::from(str)).unwrap_or(Bytes::new());
-                    Ok(Response::builder()
-                        .status(status)
-                        .header(CONTENT_TYPE, APPLICATION_JSON)
-                        .body(Full::new(Bytes::from(bytes)))
-                        .unwrap_or(Response::default()))
-                }
-                Err(_) => {
-                    let json = serde_json::to_string(
-                        &Err::<HashMap<String, HashMap<String, CachedResponse>>, String>(
-                            MockersErrors::AddMockFailed.to_string(),
-                        )
-                        .to_protocol(),
-                    );
-                    let bytes = json.map(|s| Bytes::from(s)).unwrap_or(Bytes::new());
-                    Ok(Response::builder()
-                        .status(StatusCode::INTERNAL_SERVER_ERROR)
-                        .header(CONTENT_TYPE, APPLICATION_JSON)
-                        .body(Full::new(bytes))
-                        .unwrap_or(Response::default()))
-                }
-            }
-        }
-        None => {
-            let json = serde_json::to_string(
-                &Err::<HashMap<String, HashMap<String, CachedResponse>>, String>(
-                    MockersErrors::AddMockFailed.to_string(),
-                )
-                .to_protocol(),
-            );
-            let bytes = json.map(|s| Bytes::from(s)).unwrap_or(Bytes::new());
-            Ok(Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .header(CONTENT_TYPE, APPLICATION_JSON)
-                .body(Full::new(Bytes::from(bytes)))
-                .unwrap_or(Response::default()))
-        }
-    }
+    let body_bytes = req
+        .body()
+        .clone()
+        .collect()
+        .await
+        .unwrap_or_default()
+        .to_bytes();
+    let body_str = String::from_utf8(body_bytes.to_vec()).unwrap_or(String::new());
+    let create_mock_params = serde_json::from_str::<CreateMockParams>(&body_str);
+
+    let Ok(mock_params) = create_mock_params else {
+        let json = serde_json::to_string(
+            &Err::<HashMap<String, HashMap<String, CachedResponse>>, String>(
+                MockersErrors::AddMockFailed.to_string(),
+            )
+            .to_protocol(),
+        );
+        let bytes = json.map(|s| Bytes::from(s)).unwrap_or(Bytes::new());
+        return Ok(Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .header(CONTENT_TYPE, APPLICATION_JSON)
+            .body(Full::new(bytes))
+            .unwrap_or(Response::default()));
+    };
+
+    let path = mock_params.path.clone();
+    let method = mock_params.method.clone();
+
+    // insert asynchronously
+    tokio::spawn(async move {
+        let response = &CachedResponse::new(
+            mock_params.status_code,
+            mock_params.delay_ms.unwrap_or(0),
+            mock_params.headers,
+            mock_params.body,
+        );
+        cache.upsert(&path, &method, response).await;
+    });
+
+    let json = serde_json::to_string(
+        &Ok::<String, String>(format!(
+            "Mock inserted for path: '{}' and method: '{}'",
+            mock_params.path, mock_params.method
+        ))
+        .to_protocol(),
+    );
+    let status = json
+        .as_ref()
+        .map(|_| StatusCode::OK)
+        .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
+    let bytes = json.map(|str| Bytes::from(str)).unwrap_or(Bytes::new());
+    return Ok(Response::builder()
+        .status(status)
+        .header(CONTENT_TYPE, APPLICATION_JSON)
+        .body(Full::new(Bytes::from(bytes)))
+        .unwrap_or(Response::default()));
 }
