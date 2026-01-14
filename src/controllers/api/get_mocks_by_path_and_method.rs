18cd3eb3c1a4865cb08929fa9e4dc29d39cdd35b
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2026-01-14 16:34:18 +0300

    Refactor get_mocks_by_path_and_method controller (#61)

--- src/controllers/api/get_mocks_by_path_and_method.rs
+++ src/controllers/api/get_mocks_by_path_and_method.rs
@@ -19,51 +19,46 @@ pub async fn get_mocks_by_path_and_method(
         .params()
         .get("path_encoded")
         .and_then(|str| str.decode_path().ok())
-        .unwrap_or(String::new())
+        .unwrap_or_default()
         .normalize_path();
     let method = req
         .params()
         .get("method")
         .map(|v| v.to_owned())
-        .unwrap_or(String::new())
+        .unwrap_or_default()
         .normalize_method();
 
     let response_cache = req
         .data::<Arc<MockersContext>>()
         .map(|c| c.clone().response_cache.clone());
 
-    match response_cache {
-        Some(cache) => {
-            let mocks_by_path_and_method = cache.get_mock_by_path_and_method(path, method).await;
-            let json = serde_json::to_string(
-                &Ok::<Option<CachedResponse>, String>(mocks_by_path_and_method).to_protocol(),
-            );
-            let status_code = json
-                .as_ref()
-                .map(|_| StatusCode::OK)
-                .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
-            let bytes = json
-                .map(|str| Bytes::from(str))
-                .unwrap_or(Bytes::from(Bytes::new()));
-            Ok(Response::builder()
-                .status(&status_code)
-                .header(CONTENT_TYPE, APPLICATION_JSON)
-                .body(Full::new(bytes))
-                .unwrap_or(Response::default()))
-        }
-        None => {
-            let json = serde_json::to_string(
-                &Err::<HashMap<String, HashMap<String, CachedResponse>>, String>(
-                    MockersErrors::GetMocksByPathAndMethodFailed.to_string(),
-                )
-                .to_protocol(),
-            );
-            let bytes = json.map(|s| Bytes::from(s)).unwrap_or(Bytes::new());
-            Ok(Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .header(CONTENT_TYPE, APPLICATION_JSON)
-                .body(Full::new(Bytes::from(bytes)))
-                .unwrap_or(Response::default()))
-        }
-    }
+    let Some(cache) = response_cache else {
+        let json = serde_json::to_string(
+            &Err::<HashMap<String, HashMap<String, CachedResponse>>, String>(
+                MockersErrors::GetMocksByPathAndMethodFailed.to_string(),
+            )
+            .to_protocol(),
+        );
+        let bytes: Bytes = json.map(|s| s.into()).unwrap_or_default();
+        return Ok(Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .header(CONTENT_TYPE, APPLICATION_JSON)
+            .body(bytes.into())
+            .unwrap_or_default());
+    };
+
+    let mocks_by_path_and_method = cache.get_mock_by_path_and_method(path, method).await;
+    let json = serde_json::to_string(
+        &Ok::<Option<CachedResponse>, String>(mocks_by_path_and_method).to_protocol(),
+    );
+    let status_code = json
+        .as_ref()
+        .map(|_| StatusCode::OK)
+        .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
+    let bytes: Bytes = json.map(|str| str.into()).unwrap_or_default();
+    Ok(Response::builder()
+        .status(&status_code)
+        .header(CONTENT_TYPE, APPLICATION_JSON)
+        .body(bytes.into())
+        .unwrap_or_default())
 }
