4ef8b95385463f0962df1bd64db14379148ed724
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2026-01-14 17:20:52 +0300

    Refactor delete_remove_mock_by_path_and_method (#64)

--- src/controllers/api/delete_remove_mock_by_path_and_method.rs
+++ src/controllers/api/delete_remove_mock_by_path_and_method.rs
@@ -18,59 +18,53 @@ pub async fn delete_remove_mock_by_path_and_method(
         .params()
         .get("path_encoded")
         .and_then(|str| str.decode_path().ok())
-        .unwrap_or(String::new())
+        .unwrap_or_default()
         .normalize_path();
 
     let method = req
         .params()
         .get("method")
         .map(|v| v.to_owned())
-        .unwrap_or(String::new())
+        .unwrap_or_default()
         .normalize_method();
 
     let response_cache = req
         .data::<Arc<MockersContext>>()
         .map(|c| c.clone().response_cache.clone());
 
-    match response_cache {
-        Some(cache) => {
-            let p = path.clone();
-            let m = method.clone();
-            tokio::spawn(async move {
-                cache.remove_by_path_and_method(p, m).await;
-            });
-            let json = serde_json::to_string(
-                &Ok::<String, String>(
-                    format!(
-                        "Removed mocks for path: '{}' and method: '{}'",
-                        path, method
-                    )
-                    .to_string(),
-                )
+    let Some(cache) = response_cache else {
+        let json = serde_json::to_string(
+            &Err::<String, String>(MockersErrors::RemoveMocksFailedByPathAndMethod.to_string())
                 .to_protocol(),
-            );
-            let status = json
-                .as_ref()
-                .map(|_| StatusCode::OK)
-                .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
-            let bytes = json.map(|str| Bytes::from(str)).unwrap_or(Bytes::new());
-            Ok(Response::builder()
-                .status(status)
-                .header(CONTENT_TYPE, APPLICATION_JSON)
-                .body(Full::new(Bytes::from(bytes)))
-                .unwrap_or(Response::default()))
-        }
-        None => {
-            let json = serde_json::to_string(
-                &Err::<String, String>(MockersErrors::RemoveMocksFailedByPathAndMethod.to_string())
-                    .to_protocol(),
-            );
-            let bytes = json.map(|str| Bytes::from(str)).unwrap_or(Bytes::new());
-            Ok(Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .header(CONTENT_TYPE, APPLICATION_JSON)
-                .body(Full::new(Bytes::from(bytes)))
-                .unwrap_or(Response::default()))
-        }
-    }
+        );
+        let bytes: Bytes = json.map(|str| str.into()).unwrap_or_default();
+        return Ok(Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .header(CONTENT_TYPE, APPLICATION_JSON)
+            .body(bytes.into())
+            .unwrap_or_default());
+    };
+
+    let p = path.clone();
+    let m = method.clone();
+    tokio::spawn(async move {
+        cache.remove_by_path_and_method(p, m).await;
+    });
+    let json = {
+        let message = format!(
+            "Removed mocks for path: '{}' and method: '{}'",
+            path, method
+        );
+        serde_json::to_string(&Ok::<String, String>(message).to_protocol())
+    };
+    let status = json
+        .as_ref()
+        .map(|_| StatusCode::OK)
+        .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
+    let bytes: Bytes = json.map(|str| str.into()).unwrap_or_default();
+    Ok(Response::builder()
+        .status(status)
+        .header(CONTENT_TYPE, APPLICATION_JSON)
+        .body(bytes.into())
+        .unwrap_or_default())
 }
