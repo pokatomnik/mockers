9b631ea3ba28cf5410b96884cd0ebdea4bb2671c
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2026-01-22 20:57:41 +0300

    Fix JSON mime detection

--- src/libs/get_mime.rs
+++ src/libs/get_mime.rs
@@ -1,5 +1,10 @@
-use mimetype_detector::detect;
+use mimetype_detector::{APPLICATION_JSON, detect};
+use serde::de::IgnoredAny;
 
-pub fn get_mime(source: &Vec<u8>) -> String {
-    return detect(source).mime().to_string();
+pub fn get_mime(source: &[u8]) -> String {
+    if serde_json::from_slice::<IgnoredAny>(&source).is_ok() {
+        APPLICATION_JSON.to_owned()
+    } else {
+        detect(source).mime().to_string()
+    }
 }
